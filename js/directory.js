f2a6984a79a404f841decf08177b03e9dfb6a54c
MikeSadler84 <61479491+MikeSadler84@users.noreply.github.com>
2020-08-11 17:44:47 -0500

    Updated the style
    Changed how the pets in queue were displayed above the pet information cards

--- js/directory.js
+++ js/directory.js
@@ -5,12 +5,21 @@ function display(){
 
 var pets = salon.pets;
 const petSection = document.querySelector("#pets-section");
+let petimg;
+
 var text="";
 for(var i=0;i<pets.length;i++){
-    console.log(pets[i]);
+    if(pets[i].type === "Dog"){
+        petimg = 'img/dogcard.png';
+      }else if(pets[i].type === "Cat"){  
+        petimg ='img/cat.png';
+      }else{ 
+        petimg ='img/bird.png';
+      }
     text += 
         `<div class="pet-info">
             <h2> ${pets[i].petName} </h2>
+            <img src="${petimg}">
             <p> Age: ${pets[i].age} </p>
             <p> Breed: ${pets[i].breed} </p>
             <p> Type: ${pets[i].type} </p>
@@ -18,6 +27,7 @@ for(var i=0;i<pets.length;i++){
             <p> Service: ${pets[i].service} </p>
             <p> Owner: ${pets[i].ownerName} </p>
             <p> Contact: ${pets[i].contactPhone} </p>
+            <p> Status: ${pets[i].customerType} </p>
         </div>`;
 }
 
@@ -25,3 +35,47 @@ petSection.innerHTML= text;
 };
 
 display();
+
+
+
+function numberOfPets(){
+  const numEachPet = salon.pets;
+  const filterCat = numEachPet.filter(function(numEachCat) {
+    return numEachCat.type === "Cat";
+  });
+  //Dog
+  const filterDog = numEachPet.filter(function(numEachDog) {
+  return numEachDog.type === "Dog";
+  });
+  //Bird
+  const filterBird = numEachPet.filter(function(numEachBird) {
+  return numEachBird.type === "Bird";
+  });
+  var pets = salon.pets;
+  const numberOfPets = document.querySelector("#pets-section-header");
+  let text="";
+  for(var i=0;i<pets.length;i++){
+  text=
+  `
+  <div id = "#pets-section-header">
+  <p>Pets in queue: ${pets.length}</p>
+  </div>
+  <div class ="num-of-pets-wrap">
+  <img src="../img/dogcard.png">
+  <p class="dog-img" "id="Dog">${filterDog.length}</p>
+  </div>
+  <div class ="num-of-pets-wrap">
+  <img class="cat-img" src="../img/cat.png">
+  <p id="Cat">${filterCat.length}</p>
+  </div>
+  <div class ="num-of-pets-wrap">
+  <img class="bird-img" src="../img/bird.png">
+  <p id="Bird">${filterBird.length}</p>
+  </div>
+  `;
+  }
+  numberOfPets.innerHTML= text;
+
+};
+numberOfPets();
+
--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ document.getElementById("info").innerHTML=`
 //obj constructor for the pets
 
 class Pet{
-    constructor(petName, age, breed, type, gender, service, ownerName, contactPhone){
+    constructor(petName, age, breed, type, gender, service, ownerName, contactPhone, customerType){
         this.petName=petName;
         this.age=age;
         this.breed=breed;
@@ -28,6 +28,7 @@ class Pet{
         this.service=service;
         this.ownerName=ownerName;
         this.contactPhone=contactPhone;
+        this.customerType=customerType;
     }
 }
 
@@ -35,15 +36,15 @@ class Pet{
 
 
 
-const scooby=new Pet("Scooby", 50, "Dane", "Dog", "Male", "Full Service", "Shaggy", "555-555-5555");
+const scooby=new Pet("Scooby", 50, "Dane", "Dog", "Male", "Full Service", "Shaggy", "555-555-5555", "New Customer");
 
-const scrappy=new Pet("Scrappy", 5, "Dane", "Dog", "Male", "Nails Cut", "Shaggy", "666-666-6666");
+const scrappy=new Pet("Scrappy", 5, "Dane", "Dog", "Male", "Nails Cut", "Shaggy", "666-666-6666", "New Customer");
 
-const lady=new Pet("Lady", 10, "Corgi", "Dog", "Female", "Hair Cut", "Fred", "777-777-7777");
+const lady=new Pet("Lady", 10, "Corgi", "Dog", "Female", "Hair Cut", "Fred", "777-777-7777", "Registered");
 
-const tramp=new Pet("Tramp", 15, "Mutt", "Dog", "Male", "Shots", "Velma", "888-888-8888");
+const tramp=new Pet("Tramp", 15, "Mutt", "Dog", "Male", "Shots", "Velma", "888-888-8888", "Registered");
 
-const cali=new Pet("Cali", 4, "Calico", "Cat", "Female", "De-clawing", "Jim Carrey", "999-999-9999");
+const cali=new Pet("Cali", 4, "Calico", "Cat", "Female", "De-clawing", "Jim Carrey", "999-999-9999", "Registered");
 
 
 //add pets to array
@@ -68,23 +69,23 @@ let gendertxt = document.getElementById("petGender");
 let servicetxt = document.getElementById("petService");
 let ownertxt = document.getElementById("ownerName");
 let phonetxt = document.getElementById("phoneNumber");
-
+let customertxt = document.getElementById("customerType");
 
 // Register function
 
 function register(){
 
     //create the pet object
-    const petNew=new Pet(pettxt.value, agetxt.value, breedtxt.value, typetxt.value, gendertxt.value, servicetxt.value, ownertxt.value, phonetxt.value);
+    const petNew=new Pet(pettxt.value, agetxt.value, breedtxt.value, typetxt.value, gendertxt.value, servicetxt.value, ownertxt.value, phonetxt.value, customertxt.value);
     //push the pet into the array
     salon.pets.push(petNew);
     console.log(salon.pets);
     //clear the input
     clear();
-    //display number of pets
-    numberOfPets();
     //display the newly registered pet
     display();
+    //display number of pets
+    numberOfPets();
 
 }
 
@@ -97,6 +98,7 @@ function clear(){
     servicetxt.value="";
     ownertxt.value="";
     phonetxt.value="";
+    customertxt.value="";
 }
 console.log(salon.pets);
 
@@ -105,23 +107,35 @@ console.log(salon.pets);
 //for loop
 
 
-// A function for displaying the number of pets in queue
+// function numberOfPets(){
 
+//     var pets = salon.pets;
+//     const numberOfPets = document.querySelector("#pets-section-header");
+//     let text="";
+//     for(var i=0;i<pets.length;i++){
+//     text=
+//     `
+//     <div class ="num-of-pets-wrap">
+    
 
+//     <img src="../img/dogcard.png">
+//     <p class="dog-img" "id="Dog">${filterDog.length}</p>
+//     </div>
 
-function numberOfPets(){
+//     <div class ="num-of-pets-wrap">
+//     <img class="cat-img" src="../img/cat.png">
+//     <p id="Cat">${filterCat.length}</p>
+//     </div>
 
-    var pets = salon.pets;
-    const numberOfPets = document.querySelector("#pets-section-header");
-    text=
-    `<div class="pet-section-header">
-    <h2>Pets in queue: ${pets.length}</h2>
-    </div`;
-    numberOfPets.innerHTML= text;
+//     <div class ="num-of-pets-wrap">
+//     <img class="bird-img" src="../img/bird.png">
+//     <p id="Bird">${filterBird.length}</p>
+//     </div>`;
+//     }
+//     numberOfPets.innerHTML= text;
 
-};
-
-numberOfPets();
+// };
+// numberOfPets();
 
 // A loop to pull just pet names
 
@@ -152,19 +166,66 @@ let namePet = petNames();
 // A loop to pull the age of the pet
 
 
-function petAge(){
+// function petAge(){
 
-    let agePet=0;
-    for(var i=0; i<salon.pets.length; i++){
-        agePet=salon.pets[i].age;
-        console.log(salon.pets[i].age);
-    }
-        return agePet;
-}
-let agePet = petAge();
+//     let agePet=0;
+//     for(var i=0; i<salon.pets.length; i++){
+//         agePet=salon.pets[i].age;
+//         console.log(salon.pets[i].age);
+//     }
+//         return agePet;
+// }
+// let agePet = petAge();
+
+
+
+
+/*
+
+
+
+
+
+
+Loops and functions for practice
+
+
+
+
+
+
+*/
+
+
+
+
+// let typeOfPet = () => {
+//     for(var i=0; i<salon.pets.length; i++)
+//     if(pets[i].type === "Dog"){
+//         let dogimg = document.getElementById("pets-section");
+//         dogimg.style.backgroundImage="img/dogcard.png";
+//       }else if(pets[i].type === "Cat"){
+//           let catimg = document.getElementById("pets-section");
+//           catimg.style.backgroundImage="img/cat.png";
+//       }else{
+//           let birdimg = document.getElementById("pets-section");
+//           birdimg.style.backgroundImage="img/bird.png";
+//       }
+     
+//     }
+
 
 // oldest and youngest pet for practice
 
+// function maxAge(){
+//     let maxMinAgePet=0;
+//     for(var i=0; i<salon.pets.length; i++){
+//         maxMinAgePet=salon.pets[i].age;
+//         console.log(maxMinAgePet);
+//     }
+//     return maxMinAgePet
+// }
+// let maxMinAgePet = maxAge();
 
 // A loop to pull the pets breed
 
